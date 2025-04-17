bb37ce5dbeda4a7182a28ba2cbbfe922188bd1d5
Kolea <minakwon1998@gmail.com>
2025-04-17 23:47:08 +0300

    homework #1

---
+++ src/java/homework1/Main.java
@@ -0,0 +1,17 @@
+package homework1;
+
+public class Main {
+   public static void main(String[]args) {
+       long currentWorldPopulation = 7811080815L;
+       int carsProducedThisYear = 55235358;
+       char firstLetterInAlphabet = 'A';
+       boolean javaIsAmazing = true;
+       double pi = 3.14159;
+       System.out.println("Current World Population: " + currentWorldPopulation);
+       System.out.println("Cars Produced This Year: " + carsProducedThisYear);
+       System.out.println("First Letter In Alphabet: " + firstLetterInAlphabet);
+       System.out.println("Java Is Amazing: " + javaIsAmazing);
+       System.out.println("Pi: " + pi);
+
+    }
+}
---
+++ src/java/homework2/TaxCalculator.java
@@ -0,0 +1,15 @@
+package homework2;
+
+public class TaxCalculator {
+    public static void main(String[]args) {
+        double price = 20.0;
+        double tax = 1.2;
+        int quantity = 3;
+        double total = 0;
+        String message = "I want to buy " + quantity + quantity + "shirt";
+                System.out.println(message);
+        total = price * quantity * tax;
+        System.out.println("Total cost with tax is: "+ total);
+    }
+
+}
